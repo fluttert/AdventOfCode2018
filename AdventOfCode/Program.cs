c65dad491cbc0433cccd20e2752971b583d130c5
Ernst Fluttert <e.fluttert@gmail.com>
2018-12-11 18:33:48 +0100

    Day10 solved

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -12,30 +12,18 @@ namespace AdventOfCode
             var stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            new Day08().Solve();
-            Console.WriteLine($"Day08 solved in {stopwatch.ElapsedMilliseconds}ms");
-
+            new Day10().Solve();
+            //new Day09().Solve();
+            //new Day08().Solve();
             //new Day07().Solve();
-            //Console.WriteLine($"Day07 solved in {stopwatch.ElapsedMilliseconds}ms");
-
             //new Day06().Solve();
-            //Console.WriteLine($"Day06 solved in {stopwatch.ElapsedMilliseconds}ms");
-
             //new Day05().Solve();
-            //Console.WriteLine($"Day05 solved in {stopwatch.ElapsedMilliseconds}ms");
-
             //new Day04().Solve();
-            //Console.WriteLine($"Day04 solved in {stopwatch.ElapsedMilliseconds}ms");
-
             //new Day03().Solve();
-            //Console.WriteLine($"Day03 solved in {stopwatch.ElapsedMilliseconds}ms");
-
             //new Day02().Solve();
-            //Console.WriteLine($"Day02 solved in {stopwatch.ElapsedMilliseconds}ms");
-
             //new Day01().Solve();
-            //Console.WriteLine($"Day01 solved in {stopwatch.ElapsedMilliseconds}ms");
 
+            Console.WriteLine($"Got it solved in {stopwatch.ElapsedMilliseconds}ms");
             Console.ReadLine();
         }
 
