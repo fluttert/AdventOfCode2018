2ed29086fd375ba2944ff17f7419c27b1550090e
Ernst Fluttert <e.fluttert@gmail.com>
2018-12-12 21:05:11 +0100

    Solved Day 12 Part 2

--- AdventOfCode/2018/Day12.cs
+++ AdventOfCode/2018/Day12.cs
@@ -12,33 +12,69 @@ namespace AdventOfCode._2018
             var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
             var initialLine = lines[0].Replace("initial state: ", string.Empty);
             var patterns = new Dictionary<string, char>();
-            
-            char[] state = new char[initialLine.Length * 3]; // we not expect to grow further then three times the input
-            for (int i = 0; i < state.Length; i++) { state[i] = '.'; };
-            for (int i = 0; i < initialLine.Length; i++) { state[initialLine.Length + i] = initialLine[i]; }
+
+            //char[] state = new char[4 + initialLine.Length * 2]; // we not expect to grow further then three times the input
+            //
+            var state = new List<char>() { '.', '.', '.' };
+            for (int i = 0; i < initialLine.Length; i++) { state.Add(initialLine[i]); }
+            state.Add('.'); state.Add('.'); state.Add('.'); state.Add('.');
+
             for (int i = 1; i < lines.Length; i++) {
                 string[] pattern = lines[i].Split(new char[] { ' ', '=', '>' }, StringSplitOptions.RemoveEmptyEntries);
                 patterns.Add(pattern[0], pattern[1][0]);
             }
 
             // iterations
-            string currentState = new string(state);
-            int totalPots = 0;
-            for (int i = 0; i < 200; i++) {
-                for (int j = 0; j< state.Length; j++) { state[j] = '.'; };
+            string currentState = new string(state.ToArray());
+            long stableFrom = 0;
+            //Console.WriteLine("    "+currentState);
+            int totalPotsAfter20Iterations = 0;
+            for (int i = 0; i < 500; i++) {
                 for (int j = 0; j < currentState.Length - 4; j++) {
                     var curPattern = currentState.Substring(j, 5);
-                    if (patterns.ContainsKey(curPattern)) {
+                    if (patterns.ContainsKey(curPattern))
+                    {
                         state[j + 2] = patterns[curPattern];
+                        if (j == (currentState.Length - 5)) {
+                            state.Add('.'); // extend the list!
+                        }
+                    }
+                    else { state[j + 2] = '.'; }
+
+                    
+                }
+
+                // part 02 - see when stabilised 
+                var newCurrentState = new string(state.ToArray());
+                if (newCurrentState.Substring(1) == currentState) {
+                    // break;
+                    Console.WriteLine("Stable from iteration:"+i);
+                    stableFrom = i;
+                    break;
+                }
+                currentState = new string(state.ToArray());
+
+                // part 01 
+                if (i == 19) {
+                    for (int j = 0; j < currentState.Length; j++)
+                    {
+                        if (currentState[j] == '#') { totalPotsAfter20Iterations += (j - 3); }
                     }
                 }
-                currentState = new string(state);
-                Console.WriteLine($"{i} - {currentState}");
+                //Console.WriteLine($"{i} - {currentState}"); // print em out!
             }
-            for (int i = 0; i < currentState.Length; i++) {
-                if (currentState[i] == '#') { totalPots += (i - initialLine.Length); }
+
+            // 
+            long totalPotsAfter50Billion = 0;
+            long indexShift = 50_000_000_000 - stableFrom;
+            for (int i = 0; i < currentState.Length; i++)
+            {
+                if (currentState[i] == '#') { totalPotsAfter50Billion += indexShift + (i - 3); }
             }
-            Console.WriteLine($"Day12 part1 answer: {totalPots}");
+            Console.WriteLine($"Day12 part1 answer: {totalPotsAfter20Iterations}");
+            Console.WriteLine($"Day12 part2 answer: {totalPotsAfter50Billion}");
+
+
         }
 
         public string testinput = @"initial state: #..#.#..##......###...###
