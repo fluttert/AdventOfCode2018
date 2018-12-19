0d3437e34749283573b2ac30a56dd5f1d6a65542
Ernst Fluttert <e.fluttert@gmail.com>
2018-12-19 17:45:44 +0100

    Day 09 implemented

---
+++ AdventOfCode/2018/Day09.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2018
+{
+    public class Day09
+    {
+        public LinkedList<int> game = new LinkedList<int>() { };
+
+        public void Solve()
+        {
+            string[] inputdata = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int players = int.Parse(inputdata[0]);
+            int marbleMaxWorth = int.Parse(inputdata[6]);
+
+            game.AddFirst(0);
+            LinkedListNode<int> currentNode = game.Find(0);
+            Dictionary<int, long> scoreboard = Enumerable.Range(0, players).ToDictionary(key => key, val => 0L);
+
+            for (int i = 1; i <= marbleMaxWorth; i++)
+            {
+                if (i % 23 == 0)
+                {
+                    int playerId = i % players;
+
+                    //scoreboard[playerId] += i;
+                    currentNode = currentNode.Previous == null ? game.Last : currentNode.Previous;
+                    currentNode = currentNode.Previous == null ? game.Last : currentNode.Previous;
+                    currentNode = currentNode.Previous == null ? game.Last : currentNode.Previous;
+                    currentNode = currentNode.Previous == null ? game.Last : currentNode.Previous;
+                    currentNode = currentNode.Previous == null ? game.Last : currentNode.Previous;
+                    currentNode = currentNode.Previous == null ? game.Last : currentNode.Previous;
+                    currentNode = currentNode.Previous == null ? game.Last : currentNode.Previous;
+                    int valueToRemove = currentNode.Value;
+                    currentNode = currentNode.Next == null ? game.First : currentNode.Next;
+                    scoreboard[playerId] += i + valueToRemove;
+                    game.Remove(valueToRemove);
+                }
+                else
+                {
+                    // skip 1 node and add after this
+                    currentNode = currentNode.Next == null ? game.First : currentNode.Next;
+                    game.AddAfter(currentNode, i);
+                    currentNode = currentNode.Next;
+                }
+            }
+            long highScore = scoreboard.Max(kvp => kvp.Value);
+
+            Console.WriteLine($"Day09 part1 answer: {highScore}");
+        }
+
+        public string input = @"411 players; last marble is worth 7105800 points";
+        public string testinput2 = @"9 players; last marble is worth 25 points"; // : high score is 8317
+        public string testinput = @"10 players; last marble is worth 1618 points"; // : high score is 8317
+    }
+}
